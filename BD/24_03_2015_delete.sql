71af962fb38aa8d397d96aa8894933aaa5ce4e4a
raphaelhamonnais <raphael.hamonnais@gmail.com>
2015-03-27 20:12:06 +0100

    Script Delete from table & drop sequence

---
+++ BD/24_03_2015_delete.sql
@@ -0,0 +1,26 @@
+
+
+
+drop sequence sem;
+drop sequence num;
+drop sequence id;
+drop sequence gr;
+drop sequence etu;
+drop sequence ens;
+drop sequence note;
+
+
+delete from STATS_SEMESTRE_ETUDIANT;
+delete from STATS_ENSEIGNEMENT_ETUDIANT;
+delete from NOTES;
+delete from TYPE_NOTE;
+delete from ETUDIANT_DANS_GROUPE;
+delete from ETUDIANT;
+delete from GROUPE_SUIT_ENSEIGNEMENT;
+delete from GROUPE;
+delete from ENSEIGNEMENT;
+delete from MATIERE;
+delete from SEMESTRE;
+delete from FORMATION;
+delete from PROFESSEUR;
+delete from UTILISATEUR;
