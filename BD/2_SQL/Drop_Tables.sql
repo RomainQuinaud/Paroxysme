086509a1f4824a36e818aed526acf713eea3af9d
raphaelhamonnais <raphael.hamonnais@gmail.com>
2015-04-03 12:21:36 +0200

    Changement noms fichiers et ajout Drop Table

---
+++ BD/2_SQL/Drop_Tables.sql
@@ -0,0 +1,14 @@
+drop table STATS_SEMESTRE_ETUDIANT;
+drop table STATS_ENSEIGNEMENT_ETUDIANT;
+drop table NOTES;
+drop table TYPE_NOTE;
+drop table ETUDIANT_DANS_GROUPE;
+drop table ETUDIANT;
+drop table GROUPE_SUIT_ENSEIGNEMENT;
+drop table GROUPE;
+drop table ENSEIGNEMENT;
+drop table MATIERE;
+drop table SEMESTRE;
+drop table FORMATION;
+drop table PROFESSEUR;
+drop table UTILISATEUR;
